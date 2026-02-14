cfd8a933aaf2e509c047ad72abecc0b95251d37c
Ihor Kotliarevskyi <jagor0707@gmail.com>
2026-02-14 13:50:47 +0200

    update noteId

--- src/server.js
+++ src/server.js
@@ -32,10 +32,10 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/notes/:noteId', (req, res) => {
-  const id_param = req.params.noteId;
+  const id = req.params.noteId;
 
   res.status(200).json({
-    message: `Retrieved note with ID: ${id_param}`,
+    message: `Retrieved note with ID: ${id}`,
   });
 });
 
