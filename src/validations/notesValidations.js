3207dd3c23fc5d8aa90c36fc196116e52401df14
Ihor Kotliarevskyi <jagor0707@gmail.com>
2026-02-24 19:44:04 +0200

    add pagination

--- src/validations/notesValidations.js
+++ src/validations/notesValidations.js
@@ -1,5 +1,25 @@
 import { Joi, Segments } from 'celebrate';
+import { isValidObjectId } from 'mongoose';
 import { TAGS } from '../constants/tags.js';
+
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
+
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string().valid(TAGS),
+    search: Joi.string(),
+  }),
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
 
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
@@ -21,6 +41,9 @@ export const createNoteSchema = {
 };
 
 export const updateNoteSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).messages({
       'string.base': 'Title must be a string',
@@ -35,8 +58,5 @@ export const updateNoteSchema = {
         'string.base': 'Tag must be a string',
         'any.only': `Gender must be one of: ${TAGS}`,
       }),
-  }),
-  [Segments.PARAMS]: Joi.object({
-    noteId: Joi.string().required(),
-  }),
+  }).min(1),
 };
