c36c0160dc58d44e98743ca4edd78c48fd9a56e4
Ihor Kotliarevskyi <jagor0707@gmail.com>
2026-02-28 14:58:14 +0200

    03 validation (#7)
    * init branch and add new file
    
    * add tags for validation
    
    * add validation schema for request body
    
    * add pagination
    
    * Done

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errors } from 'celebrate';
 import { errorHandler } from './middleware/errorHandler.js';
 
 import notesRoutes from './routes/notesRoutes.js';
@@ -19,6 +20,7 @@ app.use(cors());
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
+app.use(errors());
 app.use(errorHandler);
 
 await connectMongoDB();
