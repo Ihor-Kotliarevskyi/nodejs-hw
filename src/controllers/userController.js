77cf8bd6d24508569aaecf3fa681bdbcbc0585b6
Ihor Kotliarevskyi <jagor0707@gmail.com>
2026-03-24 07:10:25 +0200

    update auth

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import createHttpError from 'http-errors';
 import { updateUserAvatarUrl } from '../services/userService.js';
+import { User } from '../models/user.js';
 
 export const updateUserAvatar = async (req, res) => {
   if (!req.file) {
@@ -9,4 +10,20 @@ export const updateUserAvatar = async (req, res) => {
   const avatarUrl = await updateUserAvatarUrl(req.user._id, req.file.buffer);
 
   res.status(200).json({ url: avatarUrl });
+};
+
+export const getMe = async (req, res) => {
+  res.status(200).json(req.user);
+};
+
+export const updateMe = async (req, res) => {
+  const { username, email } = req.body;
+  const update = {};
+  if (username !== undefined) update.username = username;
+  if (email !== undefined) update.email = email;
+
+  const user = await User.findByIdAndUpdate(req.user._id, update, {
+    new: true,
+  });
+  res.status(200).json(user);
 };
